4c191f32758f2dd2ddbb48bc5e0d1beeb1b5d13a
David de los Reyes <dlos@minsait.com>
2024-04-22 15:49:14 +0200

    First websocket commit

---
+++ src/test/java/com/curso/springboot/backend/chat/SpringBootBackendWebsocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.backend.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendWebsocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
